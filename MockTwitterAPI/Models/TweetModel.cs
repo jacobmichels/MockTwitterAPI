2bb6138aacf5bc77428c5ccb95087482804918ca
Jacob Michels <jacob.michels2025@gmail.com>
2021-02-13 21:37:48 -0500

    Add tweetmodel

---
+++ MockTwitterAPI/Models/TweetModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MockTwitterAPI.Models
+{
+    public class TweetModel
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        [StringLength(280,ErrorMessage = "A tweet cannot exceed 280 characters!")]
+        public string Content { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
